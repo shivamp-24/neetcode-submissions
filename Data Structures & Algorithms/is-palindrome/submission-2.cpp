00f8b69ae79fe1df40f613f6e529627bbe339402
Shivam Singh Parihar <122880663+shivamp-24@users.noreply.github.com>
2026-05-08 12:27:09 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    bool isAlphaNum(char ch) {
+        return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9');
+    }
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        while(left < right) {
+            if(!isAlphaNum(s[left])) {
+                left++;
+                continue;
+            }
+            if(!isAlphaNum(s[right])) {
+                right--;
+                continue;
+            }
+            if(s[left] >= 'A' && s[left] <= 'Z') s[left] = tolower(s[left]);
+            if(s[right] >= 'A' && s[right] <= 'Z') s[right] = tolower(s[right]);
+            if(s[left++] != s[right--]) return false;
+        }
+        return true;
+
+        // inbuilt functions to know of
+        // isalnum() -> directly tells whether alphanumeric
+        // tolower(), toupper() -> converts character
+    }
+};
