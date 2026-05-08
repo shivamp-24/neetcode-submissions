dbe3823baba8a6b2701dec90df70b88fd2297e83
Shivam Singh Parihar <122880663+shivamp-24@users.noreply.github.com>
2026-05-08 17:19:41 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    // vector<int> dp;
+    // int findAllWays(int curr, int n) {
+    //     if(curr > n) return 0;
+    //     if(curr == n) return 1;
+
+    //     if(dp[curr] != -1) return dp[curr];
+
+    //     return dp[curr] = findAllWays(curr + 1, n) + findAllWays(curr + 2, n);
+    // }
+    int climbStairs(int n) {
+        // dp.assign(n, -1);
+        // return findAllWays(0, n);
+
+        // tabulation
+        // vector<int> dp(n+1);
+        // dp[n] = 1;
+
+        // for(int curr = n-1; curr >= 0; curr--) {
+        //     int ways = dp[curr + 1];
+        //     if(curr + 2 <= n) ways += dp[curr + 2];
+
+        //     dp[curr] = ways;
+        // }
+
+        // return dp[0];
+
+        //space optimization
+        int next = 1, next_to_next = 1;
+        for(int curr = n-2; curr >= 0; curr--) {
+            int ways = next + next_to_next;
+            next_to_next = next;
+            next = ways;
+        }
+        return next;
+    }
+};
