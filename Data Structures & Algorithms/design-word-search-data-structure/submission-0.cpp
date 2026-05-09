3b5836131c8ec7c96b8c76859a19a477fe0ee95b
Shivam Singh Parihar <122880663+shivamp-24@users.noreply.github.com>
2026-05-09 17:44:25 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.cpp
@@ -0,0 +1,47 @@
+struct Node {
+    unordered_map<char, Node*> child;
+    bool isEnd = false;
+};
+
+class WordDictionary {
+private:
+    Node* root;
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        Node* curr = root;
+        for(char ch: word) {
+            if(curr->child.find(ch) == curr->child.end()) curr->child[ch] = new Node();
+            curr = curr->child[ch];
+        }
+        curr->isEnd = true;
+    }
+    
+    bool dfs(Node* curr, int ind, string &word) {
+        if(ind == word.size()) {
+            return curr->isEnd;
+        }
+
+        char ch = word[ind];
+        if(word[ind] != '.') {
+            if(curr->child.find(ch) == curr->child.end()) {
+                return false;
+            }
+            return dfs(curr->child[ch], ind + 1, word);
+        } else {
+            bool possible = false;
+            for(auto it: curr->child) {
+                possible |= dfs(it.second, ind + 1, word);
+            }
+            return possible;
+        }
+    }
+
+    bool search(string word) {
+        Node* curr = root;
+        return dfs(curr, 0, word);
+    }
+};
